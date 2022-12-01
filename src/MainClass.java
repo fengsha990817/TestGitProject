208f89b27eeaab17038eff01afbbf5a79a32221f
fengsha990817 <38030455+fengsha990817@users.noreply.github.com>
2022-12-01 20:29:12 +0800

    这是master分支的commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,4 +2,7 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("这是用于测试Git的第一个commit");
     }
+    public static void masterBranch(){
+        System.out.println("这是master分支的提交");
+    }
 }

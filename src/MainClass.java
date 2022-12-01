13338e68156fb65f8df0c46cec64c8c60c749a89
fengsha990817 <38030455+fengsha990817@users.noreply.github.com>
2022-12-01 20:25:29 +0800

    新分支的第一次commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,10 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("这是用于测试Git的第一个commit");
+        testFunc();
+    }
+
+    public static void testFunc(){
+        System.out.println("这是新分支的函数");
     }
 }
